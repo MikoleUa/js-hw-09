3025cc14ef8ed181691433647a8727ab30b06ef6
Коля Хмелюк <nick@MacBook-Air-Kola.local>
2022-12-16 23:36:30 +0200

    js-hw-09\1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const buttonStartRef = document.querySelector('[data-start]');
+const buttonStopRef = document.querySelector('[data-stop]');
+
+buttonStartRef.addEventListener('click', onButtonStart);
+buttonStopRef.addEventListener('click', onButtonStop);
+
+let IdInterval;
+function onButtonStart() {
+  IdInterval || (IdInterval = setInterval(changeColor, 1000));
+  (buttonStartRef.disabled = true) && (buttonStopRef.disabled = false);
+}
+function onButtonStop() {
+  clearInterval(IdInterval);
+  (buttonStopRef.disabled = true) && (buttonStartRef.disabled = false);
+}
+function changeColor() {
+  document.body.style = `background: ${getRandomHexColor()}`;
+}
